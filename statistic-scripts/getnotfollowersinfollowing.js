34f008b56c5c2afdf9ad99748dc031ebd8c05d92
Akrobate <fedorov.artiom@gmail.com>
2017-09-09 21:08:57 +0200

    Adding exclusion list to the get following not followed

--- statistic-scripts/getnotfollowersinfollowing.js
+++ statistic-scripts/getnotfollowersinfollowing.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const fs = require('fs');
+const minimist = require('minimist');
+
+let argv = minimist(process.argv.slice(2));
+
+// todo: process better argv management
+//console.log(argv);
+//process.exit(0);
 
 // Params checker
 let params = process.argv;
@@ -9,17 +16,21 @@ if (process.argv[2] === undefined || process.argv[3] === undefined) {
     process.exit(1);
 }
 
+let following_lock_file = null;
 let followers_file = process.argv[2];
 let following_file = process.argv[3];
 
+if (process.argv[4] !== undefined) {
+    following_lock_file = process.argv[4];
+}
 // loading files
 
 let followers;
 let following;
+let following_lock;
 
 console.log(followers_file);
 fs.readFile(followers_file, 'utf8', (err, followers_string) => {
-
     followers = JSON.parse(followers_string);
     fs.readFile(following_file, 'utf8', (err, following_string) => {
         following = JSON.parse(following_string);
@@ -30,8 +41,18 @@ fs.readFile(followers_file, 'utf8', (err, followers_string) => {
 //        console.log(following[179]);
 
         let delta = getNotFollowersInFollowing(followers, following);
-        console.log("Not follow me, count : " + delta.length);
-        console.log(delta);
+
+        if (following_lock_file !== null) {
+            fs.readFile(following_lock_file, 'utf8', (err, following_lock_string) => {
+                following_lock = JSON.parse(following_lock_string);
+                delta = excludeLockFollowing(delta, following_lock);
+                console.log(delta);
+                console.log("Not follow me, count but with delta: " + delta.length);
+            });
+        } else {
+            console.log(delta);
+            console.log("Not follow me, count : " + delta.length);
+        }
     });
 });
 
@@ -39,6 +60,19 @@ fs.readFile(followers_file, 'utf8', (err, followers_string) => {
 function getNotFollowersInFollowing(followers_array, following_array) {
     return following_array.filter((value, index, ar) => {
         let found = followers_array.find((val) => {
+            return (value.name == val.name);
+        });
+        if (found === undefined) {
+            return true;
+        }
+        return false;
+    });
+}
+
+
+function excludeLockFollowing(following_list, following_exclude_list) {
+    return following_list.filter((value, index, ar) => {
+        let found = following_exclude_list.find((val) => {
             return (value.name == val.name);
         });
         if (found === undefined) {
