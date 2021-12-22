3940e0ead1c4731b0004696d7e37eeb3f35042a8
Akrobate <fedorov.artiom@gmail.com>
2021-12-22 03:04:38 +0100

    Updated test pupetter code

--- test-puppeteer.js
+++ test-puppeteer.js
@@ -1,42 +1,26 @@
-const puppeteer = require('puppeteer');
+'use strict';
 
+const puppeteer = require('puppeteer');
 const auth = require('./auth');
 
+const {
+  login_step,
+} = require('./steps/LoginStep');
 
-
+const {
+  navigate_to_tag_step,
+} = require('./steps/NavigateToTagStep');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.instagram.com/');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.screenshot({path: './screenshots/buddy-screenshot-0.png'});
-  await page.waitForSelector('.bIiDR');
-  await page.screenshot({path: './screenshots/buddy-screenshot-1.png'});
-  await page.click('.bIiDR');
+    await login_step.process(page, auth);
+    await navigate_to_tag_step.process(page, '#esp32');
 
-  await page.waitForSelector('.bIiDR', { hidden: true })
-  await page.screenshot({path: './screenshots/buddy-screenshot-2.png'});
+    const data = await navigate_to_tag_step.extractVisiblePostLinks(page);
 
-
-  await page.type('input[name="username"]', auth.username);
-  await page.type('input[name="password"]', auth.password);
-
-  await page.screenshot({path: './screenshots/buddy-screenshot-3.png'});
-  
-  await page.waitForSelector('button[type="submit"]:not([disabled])')
-  await page.screenshot({path: './screenshots/buddy-screenshot-4.png'});
-  
-  await page.click('button[type="submit"]');
-  await page.screenshot({path: './screenshots/buddy-screenshot-5.png'});
-  
-  await page.waitForTimeout(3000)
-  await page.screenshot({path: './screenshots/buddy-screenshot-6.png'});
-  
-  await page.waitForTimeout(13000)
-  await page.screenshot({path: './screenshots/buddy-screenshot-7.png'});
-
-
-  await browser.close();
+    console.log(data);
+    await browser.close();
 })();
 
