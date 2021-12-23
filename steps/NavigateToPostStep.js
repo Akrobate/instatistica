11937ef9f33c3d8506970afef5c3a8762be74a59
Akrobate <fedorov.artiom@gmail.com>
2021-12-23 13:07:44 +0100

    Added like / unlike / status state

--- steps/NavigateToPostStep.js
+++ steps/NavigateToPostStep.js
@@ -19,6 +19,22 @@ class NavigateToPostStep extends AbstractStep {
         await this.screenshot(page, formated_post_url); 
     }
 
+
+    async checkIsLiked(page) {
+        const aria_label = await page.evaluate(() => {
+            return document.querySelector('.fr66n svg').getAttribute('aria-label');
+        });
+        return aria_label == 'Unlike' ? true : false;
+    }
+
+
+    async clickLikeButton(page) {
+        const result = await page.evaluate(() => {
+            return document.querySelector('.fr66n svg').parentNode.parentNode.click();
+        });
+        return result;
+    }
+
 }
 
 const navigate_to_post_step = new NavigateToPostStep();
@@ -27,3 +43,10 @@ module.exports = {
     NavigateToPostStep,
     navigate_to_post_step,
 };
+
+
+
+
+
+// document.querySelector('.fr66n svg').parentNode.parentNode.click()
+// document.querySelector('.fr66n svg').getAttribute('aria-label')
