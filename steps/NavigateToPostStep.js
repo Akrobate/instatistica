47934082673e688191f37cd0b3757bedd4eb7312
Akrobate <fedorov.artiom@gmail.com>
2021-12-27 18:22:44 +0100

    Fixed elsint of NavigateToPostStep

--- steps/NavigateToPostStep.js
+++ steps/NavigateToPostStep.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 'use strict';
 
 const {
@@ -6,6 +8,7 @@ const {
 
 class NavigateToPostStep extends AbstractStep {
 
+    // eslint-disable-next-line require-jsdoc
     constructor() {
         super();
         this.screenshot_name_prefix = 'screenshot-navigate-to-tag';
@@ -13,21 +16,34 @@ class NavigateToPostStep extends AbstractStep {
     }
 
 
+    /**
+     * @param {Object} page
+     * @param {String} post_url
+     * @returns {void}
+     */
     async process(page, post_url) {
         await page.goto(`https://www.instagram.com${post_url}`);
         const formated_post_url = post_url.replace(/\//g, '');
-        await this.screenshot(page, formated_post_url); 
+        await this.screenshot(page, formated_post_url);
     }
 
 
+    /**
+     * @param {Object} page
+     * @returns {Boolean}
+     */
     async checkIsLiked(page) {
         const aria_label = await page.evaluate(() => {
             return document.querySelector('.fr66n svg').getAttribute('aria-label');
         });
-        return aria_label == 'Unlike' ? true : false;
+        return aria_label === 'Unlike';
     }
 
 
+    /**
+     * @param {Object} page
+     * @returns {void}
+     */
     async clickLikeButton(page) {
         const result = await page.evaluate(() => {
             return document.querySelector('.fr66n svg').parentNode.parentNode.click();
@@ -36,11 +52,15 @@ class NavigateToPostStep extends AbstractStep {
     }
 
 
+    /**
+     * @param {Object} page
+     * @returns {Array}
+     */
     async extractPostTags(page) {
         const result = await page.evaluate(() => {
             return [
-                ...document.querySelectorAll('.P9YgZ')[0].querySelectorAll('.xil3i')
-            ].map((element) => element.innerText)
+                ...document.querySelectorAll('.P9YgZ')[0].querySelectorAll('.xil3i'),
+            ].map((element) => element.innerText);
         });
         const formatted_tag_list = result.map((raw_tag) => raw_tag.replace('#', '').toLowerCase());
         return formatted_tag_list;
@@ -54,10 +74,3 @@ module.exports = {
     NavigateToPostStep,
     navigate_to_post_step,
 };
-
-
-
-
-
-// document.querySelector('.fr66n svg').parentNode.parentNode.click()
-// document.querySelector('.fr66n svg').getAttribute('aria-label')
