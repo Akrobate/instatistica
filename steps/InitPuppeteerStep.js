54cc42366dc0c83664a5d31f1fe9004c39bd209a
Akrobate <fedorov.artiom@gmail.com>
2021-12-28 19:52:24 +0100

    Microfix and new step of puppeteer init

---
+++ steps/InitPuppeteerStep.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+const {
+    AbstractStep,
+} = require('./AbstractStep');
+
+class InitPuppeteerStep extends AbstractStep {
+
+    // eslint-disable-next-line require-jsdoc
+    constructor() {
+        super();
+        this.screenshot_name_prefix = 'screenshot-init-puppeteer';
+        this.activate_screen_shots = true;
+        this.browser = null;
+        this.page = null;
+    }
+
+
+    /**
+     * @param {Object} page
+     * @param {String} post_url
+     * @returns {void}
+     */
+    async process() {
+        this.browser = await puppeteer.launch();
+        this.page = await browser.newPage();
+        return this;
+    }
+
+
+    /**
+     * @returns {Object}
+     */
+    getPage() {
+        return this.page;
+    }
+
+}
+
+const init_puppeteer_step = new InitPuppeteerStep();
+
+module.exports = {
+    InitPuppeteerStep,
+    init_puppeteer_step,
+};
