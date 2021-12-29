14cffebda4d6a8b96550e5a2f6924092c40165af
Akrobate <fedorov.artiom@gmail.com>
2021-12-29 18:10:26 +0100

    Updated inclusion file to access Init step

--- steps/index.js
+++ steps/index.js
@@ -20,13 +20,20 @@ const {
     navigate_to_post_step,
 } = require('./NavigateToPostStep');
 
+const {
+    InitPuppeteerStep,
+    init_puppeteer_step,
+} = require('./InitPuppeteerStep');
+
 module.exports = {
     NavigateToTagStep,
     NavigateToProfileStep,
     LoginStep,
     NavigateToPostStep,
+    InitPuppeteerStep,
     login_step,
     navigate_to_tag_step,
     navigate_to_profile_step,
     navigate_to_post_step,
+    init_puppeteer_step,
 };
