0ce4a49f82d93ac6a9e789779657f8e36b0cf14f
Akrobate <fedorov.artiom@gmail.com>
2022-01-01 22:40:41 +0100

    Working on like sequence

--- sequences/daily-like.js
+++ sequences/daily-like.js
@@ -3,6 +3,7 @@
 const {
     init_puppeteer_step,
     login_step,
+    navigate_to_tag_step,
 } = require('../steps');
 
 const auth = require('../auth');
@@ -13,5 +14,19 @@ const auth = require('../auth');
     const page = init_puppeteer_step.getPage();
 
     await login_step.process(page, auth);
+
+    // get tags list to like
+    const tag_list = [];
+
+    for (const tag of tag_list) {
+
+        await navigate_to_tag_step.process(page, tag);
+        const recent_post_result = await navigate_to_tag_step.extractVisiblePostLinks();
+        const {
+            recent,
+        } = recent_post_result;
+
+
+    }
 
 })();
