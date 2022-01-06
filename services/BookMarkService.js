1b5c617a2353872434e2291dab299aa11b236703
Akrobate <fedorov.artiom@gmail.com>
2022-01-06 13:36:10 +0100

    Added sage tags to process method to book mak service

--- services/BookMarkService.js
+++ services/BookMarkService.js
@@ -4,6 +4,17 @@ const JsonFileRepository = require('../repositories');
 
 class BookMarkService {
 
+
+    /**
+     * @param {JsonFileRepository} json_file_repository
+     */
+    constructor(json_file_repository) {
+        this.json_file_repository = json_file_repository;
+
+        this.tags_to_process_filename = 'tags_to_process.json';
+    }
+
+
     /**
      * @static
      * @returns {JsonFile}
@@ -18,6 +29,15 @@ class BookMarkService {
         return BookMarkService.instance;
     }
 
+
+    /**
+     * @param {Array} data
+     * @returns {Void}
+     */
+    async saveTagsToProcess(data) {
+        this.json_file_repository.setFileName(this.tags_to_process_filename);
+        await this.saveData(data);
+    }
 }
 
 BookMarkService.instance = null;
