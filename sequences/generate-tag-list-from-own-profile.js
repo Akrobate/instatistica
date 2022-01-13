03fe73abc544a13e974f67eef670727c2fa53302
Akrobate <fedorov.artiom@gmail.com>
2022-01-13 13:53:08 +0100

    Working on tags consolidation

--- sequences/generate-tag-list-from-own-profile.js
+++ sequences/generate-tag-list-from-own-profile.js
@@ -8,7 +8,8 @@ const {
 const {
     init_puppeteer_step,
     login_step,
-    navigate_to_tag_step,
+    navigate_to_profile_step,
+    navigate_to_post_step,
 } = require('../steps');
 
 const auth = require('../auth');
@@ -23,6 +24,14 @@ const user_to_get_tags_from = '';
     await login_step.process(page, auth);
 
     // get tags list to like
+    await navigate_to_profile_step.process(page, user_to_get_tags_from);
+    const post_list = await navigate_to_profile_step.processGetAllPostsList();
+
+    for (const post of post_list) {
+        navigate_to_post_step.process(page, post);
+        // Extract tags
+    }
+
     const tag_list = [];
 
     // Go to own profile
