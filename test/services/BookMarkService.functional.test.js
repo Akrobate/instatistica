8440dc023eb061fd0ddeafc2bddec23f68b08531
Akrobate <fedorov.artiom@gmail.com>
2022-01-25 13:07:03 +0100

    Added scafolind for testing

--- test/services/BookMarkService.functional.test.js
+++ test/services/BookMarkService.functional.test.js
@@ -1,7 +1,24 @@
 'use strict';
 
+const {
+    BookMarkService,
+} = require('../services')
+
+const {
+    mock,
+    stub,
+} = require('sinon');
+
 
 describe('test', () => {
+
+    beforeEach(() => {
+
+    });
+
+    afterEach(() => {
+
+    });
 
     it('Should be able to save file with tags', (done) => {
         done();
