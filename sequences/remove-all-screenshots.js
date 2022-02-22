df74a558cd041df17d46ea0373ea5dfd816e136d
Akrobate <fedorov.artiom@gmail.com>
2022-02-22 13:46:42 +0100

    Added script remove all screenshots

--- sequences/remove-all-screenshots.js
+++ sequences/remove-all-screenshots.js
@@ -2,13 +2,17 @@
  * Script to remove all screenshots
  */
 
-// @todo implement
+const SCREENSHOT_FOLDER = `${__dirname}/../screenshots/`;
+const SCREENSHOT_EXTENTION = 'png';
+
+const fs = require('fs').promises;
 
 (async () => {
 
-    // List all PNG files
-
-    // foreach file remove
-
+    const data = await fs.readdir(SCREENSHOT_FOLDER);
+    const screenshot_list = data.filter((file) => file.includes(`.${SCREENSHOT_EXTENTION}`));
+    for (const screenshot_file of screenshot_list) {
+        console.log(screenshot_file);
+        await fs.unlink(`${SCREENSHOT_FOLDER}/${screenshot_file}`);
+    }
 })();
-
