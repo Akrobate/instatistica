e2f64dc3c8a8b1187321f56bf5cbee5e0e97257f
Akrobate <fedorov.artiom@gmail.com>
2022-02-23 12:47:55 +0100

    Added logger to the project

--- sequences/remove-all-screenshots.js
+++ sequences/remove-all-screenshots.js
@@ -2,17 +2,20 @@
  * Script to remove all screenshots
  */
 
+const {
+    logger,
+} = require('../logger');
+
 const SCREENSHOT_FOLDER = `${__dirname}/../screenshots/`;
 const SCREENSHOT_EXTENTION = 'png';
 
 const fs = require('fs').promises;
 
 (async () => {
-
     const data = await fs.readdir(SCREENSHOT_FOLDER);
     const screenshot_list = data.filter((file) => file.includes(`.${SCREENSHOT_EXTENTION}`));
     for (const screenshot_file of screenshot_list) {
-        console.log(screenshot_file);
+        logger.log(screenshot_file);
         await fs.unlink(`${SCREENSHOT_FOLDER}/${screenshot_file}`);
     }
 })();
