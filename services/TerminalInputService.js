479a5a317289c1f47d5a56adbfcf8afd41a58d14
Akrobate <fedorov.artiom@gmail.com>
2022-02-26 23:08:17 +0100

    Params parser scaffold

--- services/TerminalInputService.js
+++ services/TerminalInputService.js
@@ -1,19 +1,32 @@
-/*
+'use strict';
 
-// @todo: create class to manage input params
+class TerminalInputService {
 
-let params = process.argv;
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-    console.log("params missing. First param: followers file, second parameter following file");
-    process.exit(1);
+    /**
+     * @return {array}
+     */
+    extractParams() {
+
+        const params = process.argv;
+        if (process.argv[2] === undefined || process.argv[3] === undefined) {
+            console.log("params missing. First param: followers file, second parameter following file");
+            process.exit(1);
+        }
+
+        let following_lock_file = null;
+        let followers_file = process.argv[2];
+        let following_file = process.argv[3];
+
+        if (process.argv[4] !== undefined) {
+            following_lock_file = process.argv[4];
+        }
+
+    }
 }
 
-let following_lock_file = null;
-let followers_file = process.argv[2];
-let following_file = process.argv[3];
+const terminal_input_service = new TerminalInputService();
 
-if (process.argv[4] !== undefined) {
-    following_lock_file = process.argv[4];
-}
-
-*/
+export {
+    TerminalInputService,
+    terminal_input_service,
+};
