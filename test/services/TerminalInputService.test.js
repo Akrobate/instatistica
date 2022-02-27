f895d22de63174c1543f83f88edb47d8b4bc5035
Akrobate <fedorov.artiom@gmail.com>
2022-02-27 14:45:08 +0100

    Scafolded TerminalInputService test

---
+++ test/services/TerminalInputService.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {
+    TerminalInputService,
+} = require('../../src/services/TerminalInputService');
+
+describe('TerminalInputService', () => {
+
+    it('Should be able to parse input', () => {
+        const terminal_input_service = TerminalInputService.getInstance();
+    });
+});
