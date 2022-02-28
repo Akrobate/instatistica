9dd4948fc9ff2e8cab43fa310b6912d3a51feb22
Akrobate <fedorov.artiom@gmail.com>
2022-02-28 13:49:38 +0100

    ExtractParams method designs

--- services/TerminalInputService.js
+++ services/TerminalInputService.js
@@ -3,24 +3,30 @@
 class TerminalInputService {
 
     /**
+     * {
+     *      outuput: <String>
+     *      position: <Int>
+     *      required: <Bool> Default true
+     * }
+     * @param {Object} params_schema
      * @return {array}
      */
-    extractParams() {
+    extractParams(params_schema = {}) {
+
+        const {
+            params_list,
+            help_message,
+        } = params_schema;
 
         const params = process.argv;
         if (process.argv[2] === undefined || process.argv[3] === undefined) {
-            console.log("params missing. First param: followers file, second parameter following file");
+            console.log(help_message);
             process.exit(1);
         }
 
         let following_lock_file = null;
         let followers_file = process.argv[2];
         let following_file = process.argv[3];
-
-        if (process.argv[4] !== undefined) {
-            following_lock_file = process.argv[4];
-        }
-
     }
 }
 
