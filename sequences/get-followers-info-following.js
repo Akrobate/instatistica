c6e55c4438bc2c524377ea0f1f058ef5f9da3a9f
Akrobate <fedorov.artiom@gmail.com>
2022-03-15 13:28:43 +0100

    Added get follower info is following or not

---
+++ sequences/get-followers-info-following.js
@@ -0,0 +1,15 @@
+/**
+ * This script get all user follower list
+ * with parameter to know if user if user
+ * is following back or not
+ */
+
+'use strict';
+
+const auth = require('../auth');
+
+const follower_info_file = '';
+
+
+(async () => {
+})();
