61c7da173db7783c399a9349599da5fdd10fbe87
Akrobate <fedorov.artiom@gmail.com>
2023-08-07 23:39:22 +0200

    Added headless detector script

---
+++ sequences/headless-detector.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {
+    init_puppeteer_step,
+    navigate_to_url_step,
+} = require('../steps');
+
+//const headless_detector_url = 'https://infosimples.github.io/detect-headless/';
+const headless_detector_url = 'https://bot.sannysoft.com/';
+
+(async () => {
+    init_puppeteer_step.headless = false;
+    await init_puppeteer_step.process();
+    const page = init_puppeteer_step.getPage();
+
+/*
+    page.setViewport({ width: 1920, height: 1080});
+*/
+
+    await navigate_to_url_step.process(page, headless_detector_url);
+
+    await page.waitForTimeout(1000);
+
+    await navigate_to_url_step.screenshot(page, 'detects');
+
+    await page.close();
+
+    const browser = init_puppeteer_step.getBrowser();
+    await browser.close();
+
+})();
