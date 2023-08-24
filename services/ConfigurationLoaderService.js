6151d8c7eb5a786be42794c591d5427ec157483c
Akrobate <fedorov.artiom@gmail.com>
2023-08-24 12:52:24 +0200

    added usage of path in configuration service

--- services/ConfigurationLoaderService.js
+++ services/ConfigurationLoaderService.js
@@ -1,23 +1,34 @@
 'use strict';
 
 const {
+    configuration,
+} = require('../configuration');
+
+const {
     JsonFileRepository,
     FileRepository,
 } = require('../repositories');
-console.log(FileRepository)
+
+const path = require('path');
+
 /**
  * Work in progress:
  * Class that will manage all user accouts and configurations
  */
 class ConfigurationLoaderService {
 
-    constructor(json_file_repository, file_repository) {
-
-        console.log(file_repository);
+    /**
+     * @param {*} json_file_repository
+     * @param {*} file_repository
+     */
+    constructor(
+        json_file_repository,
+        file_repository
+    ) {
         this.configuration_files_path = null;
         this.json_file_repository = json_file_repository;
         this.file_repository = file_repository;
-        this.users_files_path = 'users/'
+        this.users_files_path = 'users/';
     }
 
     /**
@@ -35,13 +46,17 @@ class ConfigurationLoaderService {
     }
 
     /**
-     * 
-     * @param {String} user_configuration_name 
-     * @returns 
+     * @param {String} username
+     * @returns {Object}
      */
     async loadConfiguration(username) {
         const files = await this.file_repository.listFilesInDirectory(
-            `${this.json_file_repository.getDataFolder()}${this.users_files_path}/configurations/${username}`
+            path.join(
+                configuration.getDataFolder(),
+                this.users_files_path,
+                'configurations',
+                username
+            )
         );
         console.log(files);
         return {};
