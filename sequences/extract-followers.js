bb89c7768cf1f431789ed0965322e59706ab3656
Akrobate <fedorov.artiom@gmail.com>
2023-09-05 13:50:25 +0200

    ESLint fixes

--- sequences/extract-followers.js
+++ sequences/extract-followers.js
@@ -15,13 +15,11 @@
 
 const {
     init_puppeteer_step,
-    login_step,
-    navigate_to_profile_step,
-    navigate_to_post_step,
+    // login_step,
     extract_followers_step,
 } = require('../steps');
 
-const auth = require('../auth');
+// const auth = require('../auth');
 
 const instagram_name = 'ailab_oneart';
 
@@ -31,25 +29,23 @@ const instagram_name = 'ailab_oneart';
     await init_puppeteer_step.process();
     const page = init_puppeteer_step.getPage();
 
-    //await login_step.process(page, auth);
+    // await login_step.process(page, auth);
 
     await page.goto('https://www.instagram.com/');
     await page.waitForSelector('svg[aria-label="Search"]');
     await page.click('svg[aria-label="Search"]');
-    
+
     await page.waitForSelector('input[aria-label="Search input"]');
     await page.type('input[aria-label="Search input"]', instagram_name);
 
     await page.waitForSelector(`a[href="/${instagram_name}/"]`);
     await page.click(`a[href="/${instagram_name}/"]`);
-    
+
     // await page.waitForTimeout(200000);
     // await page.waitForSelector('a[href="/"]');
     // await page.click('a[href="/"]');
-    
+
     await extract_followers_step.process(page, instagram_name);
 
     await init_puppeteer_step.stop();
-
-
 })();
