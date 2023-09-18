81b46d84d1da09b03f0f1ffc051a8d7029b23062
Akrobate <fedorov.artiom@gmail.com>
2023-09-18 12:33:49 +0200

    Added test to cover CommandLineParamsService

--- test/services/CommandLineParamsService.functional.test.js
+++ test/services/CommandLineParamsService.functional.test.js
@@ -9,25 +9,56 @@ const {
 } = require('../../services');
 
 describe('CommandLineParamsService functional test', () => {
-    it('getParam', () => {
-        const configuration_loader_service = new CommandLineParamsService();
-        configuration_loader_service.params = {
-            test: 'value',
-        };
-        const param = configuration_loader_service.getParam('test');
-        expect(param).to.equal('value');
+    describe('getParam', () => {
+        it('getParam', () => {
+            const configuration_loader_service = new CommandLineParamsService();
+            configuration_loader_service.params = {
+                test: 'value',
+            };
+            const param = configuration_loader_service.getParam('test');
+            expect(param).to.equal('value');
+        });
+
+        it('getParam required', () => {
+            const configuration_loader_service = new CommandLineParamsService();
+            configuration_loader_service.params = {
+                not_test: 'value',
+            };
+            try {
+                configuration_loader_service.getParam('test', true);
+            } catch (error) {
+                expect(error.message).to.equal('Missing param -test');
+            }
+        });
     });
 
-    it('getParam required', () => {
-        const configuration_loader_service = new CommandLineParamsService();
-        configuration_loader_service.params = {
-            not_test: 'value',
-        };
-        try {
-            configuration_loader_service.getParam('test', true);
-        } catch (error) {
-            expect(error.message).to.equal('Missing param -test');
-        }
-    });
 
+    describe('processSchema', () => {
+        it('processSchema', () => {
+            const schema = {
+                params: {
+                    'count': {
+                        type: 'Number',
+                        required: true,
+                        help: 'Some explanation text',
+                        default: undefined,
+                    },
+                },
+                array_params: [],
+            };
+
+            const argv_seed = {
+                'count': 10,
+            };
+
+            const configuration_loader_service = new CommandLineParamsService();
+            configuration_loader_service.params = argv_seed;
+            configuration_loader_service.setCommandLineParamsSchema(schema);
+
+            const response = configuration_loader_service.processSchema();
+
+            expect(response).to.have.property('count', 10);
+
+        });
+    });
 });
